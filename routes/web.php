d248f2ed71b1ed1edd6c85aa3954197ca4432190
Otto Uys <ottouys@gmail.com>
2021-07-28 08:04:39 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // DB::listen(function($query) {
+    //     Log::info(
+    //         $query->sql,
+    //         $query->bindings,
+    //         $query->time
+    //     );
+    // });
+
+    return view('posts', [
+        'posts' => Post::with('category')->get()
+    ]);
+});
+
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
