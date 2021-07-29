6e2c8f54d5374c12ec5dc26060499c67c0d062d9
Otto Uys <ottouys@gmail.com>
2021-07-29 08:04:58 +0200

    Author

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -30,7 +31,7 @@ Route::get('/', function () {
     // });
 
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('created_at')->with('category', 'author')->get()
     ]);
 });
 
@@ -43,5 +44,12 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    // dd($author);
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
